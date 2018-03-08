e418758023c478d6a0902a085dfee97c43f4e845
Joanne Lee <vernapii@gmail.com>
2018-03-08 04:33:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-if (name === "Susan") {
-    console.log("The name is correct")
-}else {
-    console.log("Expected the name to be Susan")
-}
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
